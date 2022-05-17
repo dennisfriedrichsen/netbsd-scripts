ce13d16f7343a0d42c43e28ca62ce406db21143c
Dennis R. Friedrichsen <dennis.r.friedrichsen@gmail.com>
2022-05-17 18:36:50 -0500

    add sudo to builduserland

--- build/builduserland.sh
+++ build/builduserland.sh
@@ -4,5 +4,5 @@ LOG_FILE=~/logs/builduserland_$(date +"%FT%T").log
 
 cd /usr/src
 date >> $LOG_FILE
-nohup ./build.sh -O ../obj -T ../tools -U -u distribution >> $LOG_FILE
+nohup sudo time ./build.sh -O ../obj -T ../tools -U -u distribution >> $LOG_FILE
 date >> $LOG_FILE
