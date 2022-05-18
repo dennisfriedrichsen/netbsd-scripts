4063b5c5d6e3a2373b2d2c887d2f3f4030fcd044
Dennis R. Friedrichsen <dennis.r.friedrichsen@gmail.com>
2022-05-18 07:37:50 -0500

    add sudo to buildkernel.sh

--- build/buildkernel.sh
+++ build/buildkernel.sh
@@ -4,5 +4,5 @@ LOG_FILE=~/logs/buildkernel_$(date +"%FT%T").log
 
 cd /usr/src
 date >> $LOG_FILE
-nohup ./build.sh -O ../obj -T ../tools -U -u kernel=GENERIC >> $LOG_FILE
+nohup sudo ./build.sh -O ../obj -T ../tools -U -u kernel=GENERIC >> $LOG_FILE
 date >> $LOG_FILE
